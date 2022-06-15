a66e574625b9ab792e071a213f971a8e04a59a94
Sean Fitzgerald <seanf0617@gmail.com>
2022-06-15 19:30:28 -0400

    Added the ability to borrow books

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.urls import reverse
 import uuid
+from django.contrib.auth.models import User
+from datetime import date
 
 
 class Genre(models.Model):
@@ -46,6 +48,13 @@ class BookInstance(models.Model):
     book = models.ForeignKey('Book', on_delete=models.RESTRICT, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
+    @property
+    def is_overdue(self):
+        """Determines if the book is overdue based on due date and current date."""
+        return bool(self.due_back and date.today() > self.due_back)
+
 
     LOAN_STATUS = (
         ('m', 'Maintenance'),
