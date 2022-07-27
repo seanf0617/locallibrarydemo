92700e4d3afea18baedccc715c4bf50b7e2c9cda
Sean Fitzgerald <seanf0617@gmail.com>
2022-07-26 22:14:25 -0400

    Renew book function to Librarians and author view

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -100,7 +100,7 @@ def renew_book_librarian(request, pk):
             book_instance.save()
 
             # redirect to a new URL:
-            return HttpResponseRedirect(reverse('all-borrowed') )
+            return HttpResponseRedirect(reverse('borrowed') )
 
     # If this is a GET (or any other method) create the default form.
     else:
@@ -114,3 +114,20 @@ def renew_book_librarian(request, pk):
 
     return render(request, 'catalog/book_renew_librarian.html', context)
 
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+from catalog.models import Author
+
+class AuthorCreate(CreateView):
+    model = Author
+    fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+    initial = {'date_of_death': '11/06/2020'}
+
+class AuthorUpdate(UpdateView):
+    model = Author
+    fields = '__all__' # Not recommended (potential security issue if more fields added)
+
+class AuthorDelete(DeleteView):
+    model = Author
+    success_url = reverse_lazy('authors')
