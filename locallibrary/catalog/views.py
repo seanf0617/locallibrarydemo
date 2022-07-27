6ac0559a9042269b268f7c056114210811fb07ad
Sean Fitzgerald <seanf0617@gmail.com>
2022-07-26 22:54:59 -0400

    Added author create, update and delete.  Need sec

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -114,20 +114,25 @@ def renew_book_librarian(request, pk):
 
     return render(request, 'catalog/book_renew_librarian.html', context)
 
+
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
 from catalog.models import Author
 
+###  Add security to block unautorized use !!!  PermissionRequiredMixin and can_mark_returned
 class AuthorCreate(CreateView):
     model = Author
     fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
-    initial = {'date_of_death': '11/06/2020'}
+    success_url = reverse_lazy('authors')
 
+###  Add security to block unautorized use !!!  PermissionRequiredMixin and can_mark_returned
 class AuthorUpdate(UpdateView):
     model = Author
     fields = '__all__' # Not recommended (potential security issue if more fields added)
+#    success_url = reverse_lazy('authors')
 
+###  Add security to block unautorized use !!!  PermissionRequiredMixin and can_mark_returned
 class AuthorDelete(DeleteView):
     model = Author
     success_url = reverse_lazy('authors')
