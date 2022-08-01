6a19ef931136e82b9048970c4e9113abc478abd3
Sean Fitzgerald <seanf0617@gmail.com>
2022-08-01 14:34:17 -0400

    Added Skeleton tests

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -84,15 +84,13 @@ class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     date_of_birth = models.DateField(null=True, blank=True)
-    date_of_death = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
 
     class Meta:
         ordering = ['last_name', 'first_name']
 
     def __str__(self):
-        """String for representing the Model object."""
         return '{0}, {1}'.format(self.last_name, self.first_name, self.date_of_birth, self.date_of_death)
 
     def get_absolute_url(self):
-        """Returns the URL to access a particular author instance."""
         return reverse('author-detail', args=[str(self.id)])
